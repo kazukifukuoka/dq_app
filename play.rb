9256b62578c08b2548f49612b33ed09d1d3007a6
福岡和樹 <kazuki@mba.local>
2020-03-15 19:13:37 +0900

    dq_app

--- play.rb
+++ play.rb
@@ -1,1 +1,8 @@
-a
+require './brave.rb'
+require './monster.rb'
+
+brave = Brave.new(name: "勇者", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "モンスター", hp: 250, offense: 200, defense: 100)
+
+brave.status
+monster.status
