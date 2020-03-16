b3b2f9d012e3f752b7b73b2d6d4f26b00da556a0
福岡和樹 <kazuki@mba.local>
2020-03-16 22:38:26 +0900

    dq_app

--- monster.rb
+++ monster.rb
@@ -1,6 +1,8 @@
 require './character.rb'
 
 class Monster < Character
+  POWER_UP_RATE = 1.5
+
   def status
     puts <<~TEXT
     #{@name}
@@ -8,5 +10,51 @@ class Monster < Character
       攻撃力：#{@offense}
       守備力：#{@defense}
     TEXT
+  end
+
+  def attack(brave)
+
+    monster_type = decision_transform
+    puts "#{@name}の攻撃"
+    damage = caluculate_damage(brave)
+    cause_damage(target: brave, damage: damage)
+
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+  def decision_transform
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+      "power_up_type"
+    else
+      "previous_power_up_type"
+    end
+  end
+
+  def transform
+    transform_name = "ドラゴン"
+
+    puts <<~TEXT
+    #{@name}は怒っている
+    #{@name}は#{transform_name}に変身した
+    TEXT
+
+    @offense *= POWER_UP_RATE
+    @name = transform_name
+  end
+
+  def caluculate_damage(target)
+    @offense - target.defense
+  end
+
+  def cause_damage(**params)
+    damage = params[:damage]
+    target = params[:target]
+
+    target.hp -= damage
+    target.hp = 0 if target.hp < 0
+    puts "#{target.name}は#{damage}のダメージを受けた"
   end
 end
--- play.rb
+++ play.rb
@@ -4,5 +4,11 @@ require './monster.rb'
 brave = Brave.new(name: "勇者", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "モンスター", hp: 250, offense: 200, defense: 100)
 
-brave.status
-monster.status
+# brave.status
+# monster.status
+loop do
+brave.attack(monster)
+break if monster.hp <= 0
+monster.attack(brave)
+break if brave.hp <= 0
+end
