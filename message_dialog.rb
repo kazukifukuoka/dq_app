5913151d6d8db6dd88823787c15019b5cfe7a7f4
福岡和樹 <kazuki@mba.local>
2020-03-20 17:01:02 +0900

    dq_app

--- message_dialog.rb
+++ message_dialog.rb
@@ -10,11 +10,38 @@ module MessageDialog
     damage = params[:damage]
 
     puts <<~TEXT
+
     #{target.name}は#{damage}のダメージを受けた
     #{target.name}は#{damage}のダメージを受けた
+
     TEXT
   end
 
-  def end_message
+  def end_message(result)
+    if result[:brave_win_flag]
+      puts <<~TEXT
+
+      勇者は勝った
+      #{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した
+      TEXT
+    else
+      puts <<~TEXT
+
+      勇者は負けた
+      目の前が真っ暗になった
+
+      TEXT
+    end
+  end
+
+  def transform_message(**params)
+    origin_name = params[:origin_name]
+    transform_name = params[:transform_name]
+    puts <<~TEXT
+
+    #{origin_name}は怒っている
+    #{origin_name}は#{transform_name}に変身した
+
+    TEXT
   end
 end
